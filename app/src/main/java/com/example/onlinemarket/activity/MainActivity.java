f7b9c96d18c742610f4ab3f5e0f347c15c2a390d
AysudaBeigi <AysudaBeigi@gmail.com>
2021-02-03 20:15:49 +0330

    feat: Design HomeFragment.

--- app/src/main/java/com/example/onlinemarket/activity/MainActivity.java
+++ app/src/main/java/com/example/onlinemarket/activity/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.onlinemarket.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
 import android.content.Context;
 import android.content.Intent;
@@ -35,65 +32,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         findViews();
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().
+            getSupportFragmentManager()
+                    .beginTransaction().
                     add(R.id.fragment_container_main_activity,
                             HomeFragment.newInstance())
                     .commit();
         }
-        moveNavigationButton();
+        setListeners();
     }
 
     private void findViews() {
         mBottomNavigationView=findViewById(R.id.navigation_view_main_activity);
     }
-    private void moveNavigationButton() {
-        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private void setListeners() {
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.navigation_shoppingBag:
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_container_main_activity,
-                                        ShoppingBagFragment.newInstance())
-                                .hide(HomeFragment.newInstance())
-                                .commit();
-                        return true;
                     case R.id.navigation_profile:
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_container_main_activity,
-                                        UserProfileFragment.newInstance())
-                                .hide(HomeFragment.newInstance())
-                                .commit();
+                       replaceFragment(UserProfileFragment.newInstance());
+                        return true;
+                    case R.id.navigation_shoppingBag:
+                        replaceFragment(ShoppingBagFragment.newInstance());
                         return true;
                     case R.id.navigation_category:
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_container_main_activity,
-                                        CategoryFragment.newInstance())
-                                .hide(HomeFragment.newInstance())
-
-                                .commit();
+                        replaceFragment(CategoryFragment.newInstance());
                         return true;
-
                     case R.id.navigation_home:
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.fragment_container_main_activity,
-                                        HomeFragment.newInstance())
-                                .hide(HomeFragment.newInstance())
-
-                                .commit();
+                       replaceFragment(HomeFragment.newInstance());
                         return true;
                     default:
-                        throw new IllegalStateException("Unexpected value: " + item.getItemId());
+                        throw new IllegalStateException(
+                                "Unexpected value: " +
+                                        item.getItemId());
                 }
             }
         });
+    }
+
+    private void replaceFragment(Fragment newFragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container_main_activity,newFragment)
+                .hide(HomeFragment.newInstance())
+                .commit();
     }
 
     @Override
     public void onBackPressed() {
         Fragment fragment = getSupportFragmentManager().
                 findFragmentById(R.id.fragment_container_main_activity);
-        if (!(fragment instanceof IOnBackPress) || !((IOnBackPress) fragment).onBackPressed()) {
+        if (!(fragment instanceof IOnBackPress) ||
+                !((IOnBackPress) fragment).onBackPressed()) {
             super.onBackPressed();
         }else {
             getSupportFragmentManager().
