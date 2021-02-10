a1f64f1a5ef9a58c88d3ae1567624016ade76638
AysudaBeigi <AysudaBeigi@gmail.com>
2021-02-10 19:22:52 +0330

    feat: Add  CardAdapter .

--- app/src/main/java/com/example/onlinemarket/adapter/CartAdapter.java
+++ app/src/main/java/com/example/onlinemarket/adapter/CartAdapter.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +16,7 @@ import com.example.onlinemarket.model.Cart;
 import com.example.onlinemarket.model.product.Image;
 import com.example.onlinemarket.model.product.Product;
 import com.example.onlinemarket.repository.CartDBRepository;
-import com.google.android.material.button.MaterialButton;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.textview.MaterialTextView;
 import com.squareup.picasso.Picasso;
 
@@ -29,9 +27,8 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CardViewHolder
     public static final String TAG = "CardAdapter";
     private Context mContext;
     private List<Product> mCartProducts;
-    private int mFinalPriceValue = 0;
+    private int mSumPriceCarts = 0;
     CartDBRepository mCartDBRepository;
-
 
 
     public List<Product> getCartProducts() {
@@ -72,14 +69,15 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CardViewHolder
 
     public class CardViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView mName;
-        private MaterialButton mMaterialButtonPlus,
-                mMaterialButtonMinus, mMaterialButtonDelete;
-        private MaterialTextView mCount, mFinalPrice;
-        private ImageView mImageView;
+        private MaterialTextView mTextViewName;
+        private MaterialTextView mTextViewPlus,
+                mTextViewMinus;
+        private MaterialTextView mTextViewCount, mTextViewBasePriceCart;
+        private ShapeableImageView mImageViewProduct, mImageViewTrash;
         private Cart mCart;
         private int mProductCount;
-        private int basePrice = 0;
+        private int basePriceCart = 0;
+        private int mSumPriceCart = 0;
 
 
         public CardViewHolder(@NonNull View itemView) {
@@ -89,77 +87,69 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CardViewHolder
 
 
         private void findHolderViews(@NonNull View itemView) {
-            mName = itemView.findViewById(R.id.card_name);
-            mMaterialButtonPlus = itemView.findViewById(R.id.card_plus_button);
-            mMaterialButtonMinus = itemView.findViewById(R.id.card_minus_button);
-            mMaterialButtonDelete = itemView.findViewById(R.id.card_delete);
-            mCount = itemView.findViewById(R.id.card_count);
-            mImageView = itemView.findViewById(R.id.card_image_view);
-            mFinalPrice = itemView.findViewById(R.id.text_view_product_price);
+            mTextViewName = itemView.findViewById(R.id.card_name);
+            mTextViewPlus = itemView.findViewById(R.id.card_plus_button);
+            mTextViewCount = itemView.findViewById(R.id.card_count);
+            mTextViewMinus = itemView.findViewById(R.id.card_minus_button);
+            mImageViewTrash = itemView.findViewById(R.id.image_view_trash);
+            mImageViewProduct = itemView.findViewById(R.id.image_view_card_item);
+            mTextViewBasePriceCart = itemView.findViewById(R.id.text_view_product_price);
 
         }
 
         private void bindProduct(Product product) {
-            mCart=mCartDBRepository.getCart(product.getId());
-            mProductCount=mCart.getProductCount();
-            mName.setText(product.getName() + "");
-            mFinalPrice.setText(product.getPrice() +
-                    " " + mContext.getResources().getString(R.string.toman));
-            mCount.setText(mProductCount + "");
-            basePrice = Integer.parseInt(product.getPrice());
-            mSalePrice.setText(mProductCount * Integer.parseInt(product.getPrice())
-                    + " " + mContext.getResources().getString(R.string.toman));
-
-            mFinalPriceValue = mFinalPriceValue + mProductCount * basePrice;
+            mCart = mCartDBRepository.getCart(product.getId());
+            mProductCount = mCart.getProductCount();
+            mTextViewName.setText(product.getName());
+            basePriceCart = Integer.parseInt(product.getPrice());
+            mTextViewBasePriceCart.setText(basePriceCart
+                    + mContext.getResources().getString(R.string.toman));
+            mTextViewCount.setText(mProductCount);
+            mSumPriceCart = basePriceCart * mProductCount;
+            mSumPriceCarts = mSumPriceCarts + mSumPriceCart;
 
             List<Image> imagesItems = product.getImages();
             if (imagesItems.get(0).getSrc().length() != 0)
                 Picasso.get()
                         .load(imagesItems.get(0).getSrc())
-                        .into(mImageView);
+                        .into(mImageViewProduct);
 
         }
 
         private void setListener(int position) {
-            mMaterialButtonPlus.setOnClickListener(new View.OnClickListener() {
+            mTextViewPlus.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mCart.setProductCount(++mProductCount);
                     mCartDBRepository.updateCart(mCart);
-                    if (mProductCount <= 0)
-                        mProductCount = 0;
-                    mProductCount++;
-
-                    if (mProductCount >= 0) {
-                        mCount.setText(mProductCount + "");
-                        mSalePrice.setText(mProductCount * basePrice +
-                                " " + mContext.getResources().getString(R.string.toman));
-                        mFinalPriceValue = mFinalPriceValue + mProductCount * basePrice;
-
-                    }
+                    mTextViewCount.setText(mProductCount + "");
+                    updateSumPriceCarts();
 
                 }
             });
 
-            mMaterialButtonMinus.setOnClickListener(new View.OnClickListener() {
+            mTextViewMinus.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mProductCount--;
-                    if (mProductCount >= 0) {
-                        mCount.setText(mProductCount + "");
-                        mSalePrice.setText(mProductCount * basePrice +
-                                " " + mContext.getResources().getString(R.string.toman));
-                        mFinalPriceValue = mFinalPriceValue - mProductCount * basePrice;
+                    if (mProductCount > 0) {
+                        mTextViewCount.setText(mProductCount + "");
+                        updateSumPriceCarts();
+                    }else {
+                        mTextViewMinus.setVisibility(View.GONE);
+                        mImageViewTrash.setVisibility(View.VISIBLE);
                     }
                 }
             });
 
-            mMaterialButtonDelete.setOnClickListener(new View.OnClickListener() {
+            mImageViewTrash.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Cart cart = mCartDBRepository.getCart(mCartProducts.get(position).getId());
+                    Cart cart = mCartDBRepository.
+                            getCart(mCartProducts.get(position).getId());
                     mCartDBRepository.deleteCart(cart);
-                    ((AppCompatActivity) mContext).getSupportFragmentManager().beginTransaction()
+                    ((AppCompatActivity) mContext).getSupportFragmentManager().
+                            beginTransaction()
                             .replace(R.id.fragment_container_main_activity,
                                     ShoppingBagFragment.newInstance())
                             .commit();
@@ -168,6 +158,11 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CardViewHolder
                 }
             });
 
+        }
+
+        private void updateSumPriceCarts() {
+            mSumPriceCart = mProductCount * basePriceCart;
+            mSumPriceCarts = mSumPriceCarts + mSumPriceCart;
         }
 
     }
